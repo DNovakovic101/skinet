f765f91c0295791aef2b921b68b0737de1958603
DNovakovic101 <dusannovakovic32@gmail.com>
2022-02-18 16:20:28 +0100

    Added ngx-spinner for loading

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { NavigationExtras, Router } from '@angular/router';
-import { catchError } from 'rxjs/operators';
+import { catchError, delay } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
@@ -17,28 +17,29 @@ export class ErrorInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
-    return next.handle(request).pipe(catchError(error => {
-      if (error) {
-        if (error.status === 400) {
-          if (error.error.errors) {
-            throw error.error;
+    return next.handle(request).pipe(
+      catchError(error => {
+        if (error) {
+          if (error.status === 400) {
+            if (error.error.errors) {
+              throw error.error;
+            }
+            else {
+              this.toastr.error(error.error.message, error.error.statusCode);
+            }
           }
-          else {
+          if (error.status === 401) {
             this.toastr.error(error.error.message, error.error.statusCode);
           }
+          if (error.status === 404) {
+            this.router.navigateByUrl('/not-found');
+          }
+          if (error.status === 500) {
+            const navigationExtras: NavigationExtras = { state: { error: error.error } };
+            this.router.navigateByUrl('/server-error', navigationExtras);
+          }
+          return throwError(error);
         }
-        if (error.status === 401) {
-          this.toastr.error(error.error.message, error.error.statusCode);
-        }
-        if (error.status === 404) {
-          this.router.navigateByUrl('/not-found');
-        }
-        if (error.status === 500) {
-          const navigationExtras: NavigationExtras = { state: { error: error.error } };
-          this.router.navigateByUrl('/server-error', navigationExtras);
-        }
-        return throwError(error);
-      }
     }));
   }
 }
